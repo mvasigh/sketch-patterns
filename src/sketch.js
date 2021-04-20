1f0585ac42199cc12326789981043b6be599618a
Mehdi Vasigh <mehdi.vasigh@gmail.com>
2021-04-19 23:17:50 -0400

    fun with ideas from Martin Kleppe tweet

--- src/sketch.js
+++ src/sketch.js
@@ -1,7 +1,8 @@
 import p5 from "p5";
 
-const WIDTH = 720;
-const HEIGHT = 720;
+const WIDTH = 800;
+const HEIGHT = 800;
+let k = 3;
 
 /** @type {(p: p5) => void} */
 export default (p) => {
@@ -12,27 +13,26 @@ export default (p) => {
     p.createCanvas(WIDTH, HEIGHT);
     p.background(0);
   };
-
+  
   /**
    * Runs once for each frame
    */
   p.draw = () => {
-    p.stroke(255);
-    p.strokeWeight(1);
-    p.noFill();
-    p.circle(WIDTH / 2, HEIGHT / 2, 200);
-    p.circle(WIDTH / 2, HEIGHT / 2, 180);
-    p.circle(WIDTH / 2, HEIGHT / 2, 160);
-  };
-
-  /**
-   * Runs when an OSC message is received
-   */
-  p.onmessage = ({ address, args, timeTag }) => {
-    switch (address) {
-      case "/ping": {
-        console.log("pong!", args);
+    p.background(20);
+    p.loadPixels();
+    for (let x = 0; x < WIDTH; x++) {
+      for (let y = 0; y < HEIGHT; y++) {
+        let i = ((y * WIDTH) + x) * 4;
+        let val = (x & y) & (x ^ y) % k ? 200 : 0;
+        if (!val) {
+          continue;
+        }
+        p.pixels[i] = p.pixels[i + 1] = p.pixels[i + 2] = val;
+        p.pixels[i + 3] = val;
       }
     }
+    p.updatePixels();
+
+    k += 4;
   };
 };
